cdd198cce6e5b777711b2379faffb2af96a22f56
adenugbamichael <adenugbamicke@gmail.com>
2023-07-14 14:52:56 +0100

    fix prod...

--- server/index.js
+++ server/index.js
@@ -13,17 +13,19 @@ connectDB()
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-// app.use("/uploads", express.static("uploads"))
+app.use("/uploads", express.static("uploads"))
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve()
-  app.use("/uploads", express.static("uploads"))
+  app.use("/uploads", express.static("/var/data/uploads"))
   app.use(express.static(path.join(__dirname, "/client/dist")))
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
   )
 } else {
+  const __dirname = path.resolve()
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
   app.get("/", (req, res) => {
     res.json("Hello world")
   })
