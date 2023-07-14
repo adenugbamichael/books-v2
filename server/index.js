e45573bd164e841fd225cf5d84c7c88be6c15e1a
adenugbamichael <adenugbamicke@gmail.com>
2023-07-14 14:47:39 +0100

    fix uploads

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve()
-  app.use("/uploads", express.static("/uploads"))
+  app.use("/uploads", express.static("uploads"))
   app.use(express.static(path.join(__dirname, "/client/dist")))
 
   app.get("*", (req, res) =>
