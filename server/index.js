efc728eb6ffbdf2d516a7b9b79c52cfdb22b1547
adenugbamichael <adenugbamicke@gmail.com>
2023-07-14 13:20:22 +0100

    fix server

--- server/index.js
+++ server/index.js
@@ -17,10 +17,10 @@ app.use("/uploads", express.static("uploads"))
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve()
-  app.use(express.static(path.join(__dirname, "/frontend/dist")))
+  app.use(express.static(path.join(__dirname, "/client/dist")))
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
   )
 } else {
   app.get("/", (req, res) => {
