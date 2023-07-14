74682e94bd09d93758877c40fdc61a4f0f79dba3
adenugbamichael <adenugbamicke@gmail.com>
2023-07-14 14:42:51 +0100

    fix ./dist

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(express.json())
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve()
   app.use("/uploads", express.static("/uploads"))
-  app.use(express.static(path.join(__dirname, "/client/build")))
+  app.use(express.static(path.join(__dirname, "/client/dist")))
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
