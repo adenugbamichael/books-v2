803a7814a8e7dc9e9af5d7966b034912769e4112
adenugbamichael <adenugbamicke@gmail.com>
2023-07-14 14:29:30 +0100

    fix server

--- server/index.js
+++ server/index.js
@@ -15,17 +15,16 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 // app.use("/uploads", express.static("uploads"))
 
+const __dirname = path.resolve()
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
+
 if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve()
-  app.use("/uploads", express.static("/var/data/uploads"))
   app.use(express.static(path.join(__dirname, "/client/build")))
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   )
 } else {
-  const __dirname = path.resolve()
-  app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
   app.get("/", (req, res) => {
     res.json("Hello world")
   })
